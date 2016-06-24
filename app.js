f4b44a54b2318b36aa1b82afc221571f5a267542
demeshenko <demeshenko@neolant.ru>
2016-06-24 11:58:29 +0300

    more init

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-"use strict";
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");
-    console.log("I AM TEST ");v
+    console.log("I AM TEST 1");
 
-const Hazel = require("hazel-wiki").app;
-const config = require("./config.default.js");
-const StorageProvider = require("hazel-wiki").storageProvider;
+var Hazel = require("hazel-wiki").app;
+    console.log("I AM TEST 2");
+var config = require("./config.default.js");
+    console.log("I AM TEST 3");
+var StorageProvider = require("hazel-wiki").storageProvider;
+    console.log("I AM TEST 4");
 
-let app = new Hazel(config, StorageProvider);
-let server = app.server;
+var app = new Hazel(config, StorageProvider);
+    console.log("I AM TEST 5");
+var server = app.server;
+    console.log("I AM TEST 6");
 
-server.listen(5000, () => {
+server.listen(5000, function() {
+    console.log("I AM TEST 7");
     console.log("✔ Hazel server listening at 3000 ");
 });
